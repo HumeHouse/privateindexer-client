b9cf91e353747cb44258b42ed2963a5e7d1be3f0
David Hume <xenophilicy@gmail.com>
2025-08-25 20:31:27 -0500

    feat: new config file setup

--- src/privateindexer_client/core/config.py
+++ src/privateindexer_client/core/config.py
@@ -1,8 +1,16 @@
+import asyncio
+import json
 import os
+
+from privateindexer_client.core.logger import log
 
 # gather/set environment variables for usage later
 TORRENTS_FILE = "/app/data/torrents.json"
 TORRENTS_DIR = "/app/data/torrents"
+
+DOWNLOADS_DIR = "/app/downloads"
+
+CONFIG_FILE = "/app/data/config.json"
 
 CATEGORY_PATHS = {}
 
@@ -18,3 +26,32 @@ MOVIE_EXTENSIONS = os.getenv("MOVIE_EXTENSIONS", "mp4,mkv,m4v,avi").split(",")
 API_KEY = os.getenv("API_KEY")
 
 TORRENTING_PORT = int(os.getenv("TORRENTING_PORT", "6881"))
+
+config_lock = asyncio.Lock()
+
+
+async def load_config_threadsafe():
+    """
+    Reads data from the JSON configuration file
+    """
+    async with config_lock:
+        if not os.path.exists(CONFIG_FILE):
+            return {}
+        try:
+            with open(CONFIG_FILE, "r") as f:
+                return json.load(f)
+        except Exception as e:
+            log.error(f"[CONFIG] Failed to load config.json: {e}")
+            return {}
+
+
+async def save_config_threadsafe(config):
+    """
+    Writes data to the JSON configuration file
+    """
+    async with config_lock:
+        try:
+            with open(CONFIG_FILE, "w") as f:
+                json.dump(config, f, indent=2)
+        except Exception as e:
+            log.error(f"[CONFIG] Failed to write config.json: {e}")
