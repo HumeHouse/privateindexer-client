44e794ffcee31ff39f28aec86ef8b64a12a25f05
David Hume <xenophilicy@gmail.com>
2025-09-01 11:21:01 -0500

    feat: expose new maindata endpoint to GUI and update dashboard

--- src/privateindexer_client/core/gui.py
+++ src/privateindexer_client/core/gui.py
@@ -1,5 +1,5 @@
-from fastapi import Request, APIRouter
-from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
+from fastapi import Request, APIRouter, HTTPException
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 
 from privateindexer_client.core import torrent_client, utils
@@ -20,13 +20,30 @@ async def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", context={"request": request})
 
 
-@router.get("/dashboard/torrents")
-async def dashboard_torrents():
-    log.debug("[GUI] Torrent data fetched")
-    torrents = torrent_client.get_all_torrents()
-    if not torrents:
-        return JSONResponse({"error": "Failed to get torrents"}, status_code=500)
-    return [utils.map_torrent_to_qbit(torrent) for torrent in torrents]
+@router.get("/dashboard/maindata")
+async def dashboard_maindata():
+    log.debug("[GUI] Main data fetched")
+    main_data = {}
+
+    try:
+        torrents = torrent_client.get_all_torrents()
+        mapped = [utils.map_torrent_to_qbit(t) for t in torrents]
+
+        main_data["torrents"] = mapped
+    except Exception as e:
+        log.error(f"[GUI] Failed to get torrent list: {e}")
+        return HTTPException(status_code=500, detail="Failed to get torrent list")
+
+    try:
+        stats_now, time_now, stats_prev, time_prev = torrent_client.get_session_stats()
+        all_time_download, all_time_upload = torrent_client.get_all_time_stats()
+
+        main_data["server_state"] = utils.map_stats_to_qbit(stats_now, time_now, stats_prev, time_prev, all_time_download, all_time_upload)
+    except Exception as e:
+        log.error(f"[GUI] Failed to get session info: {e}")
+        return HTTPException(status_code=500, detail="Failed to get session info")
+
+    return main_data
 
 
 @router.get("/dashboard/user")
@@ -34,5 +51,5 @@ async def dashboard_user_stats():
     log.debug("[GUI] User statistics fetched")
     user_data = await utils.fetch_indexer_user_data()
     if not user_data:
-        return JSONResponse({"error": "Failed to fetch user stats"}, status_code=500)
+        return HTTPException(status_code=500, detail="Failed to fetch user stats")
     return user_data
