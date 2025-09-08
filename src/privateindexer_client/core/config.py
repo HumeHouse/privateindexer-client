99f183fb66d3178ddca441ac53ee52eb3cbb3642
David Hume <xenophilicy@gmail.com>
2025-09-08 11:47:03 -0500

    build: version bump

--- src/privateindexer_client/core/config.py
+++ src/privateindexer_client/core/config.py
@@ -4,7 +4,7 @@ import threading
 
 from privateindexer_client.core.logger import log
 
-APP_VERSION = "1.2.8"
+APP_VERSION = "1.3.0"
 
 # gather/set environment variables for usage later
 DATABASE_FILE = "/app/data/torrents.db"
