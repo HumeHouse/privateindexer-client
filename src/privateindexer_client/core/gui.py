ae5f6742d87d3c1de99bee8df7877b496572146d
David Hume <xenophilicy@gmail.com>
2025-10-30 23:04:15 -0500

    fix: raise HTTP errors, not return

--- src/privateindexer_client/core/gui.py
+++ src/privateindexer_client/core/gui.py
@@ -1,8 +1,12 @@
-from fastapi import Request, APIRouter, HTTPException
+import os
+
+from fastapi import Request, APIRouter, HTTPException, Query
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+from starlette.responses import PlainTextResponse
+from starlette.status import HTTP_404_NOT_FOUND, HTTP_500_INTERNAL_SERVER_ERROR
 
-from privateindexer_client.core import torrent_client, utils, scan
+from privateindexer_client.core import torrent_client, utils, scan, database
 from privateindexer_client.core.config import APP_VERSION
 from privateindexer_client.core.logger import log
 
@@ -33,7 +37,7 @@ async def dashboard_maindata():
         main_data["torrents"] = mapped
     except Exception as e:
         log.error(f"[GUI] Failed to get torrent list: {e}")
-        return HTTPException(status_code=500, detail="Failed to get torrent list")
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get torrent list")
 
     try:
         stats_now, time_now, stats_prev, time_prev = torrent_client.get_session_stats()
@@ -42,13 +46,13 @@ async def dashboard_maindata():
         main_data["server_state"] = utils.map_stats_to_qbit(stats_now, time_now, stats_prev, time_prev, all_time_download, all_time_upload)
     except Exception as e:
         log.error(f"[GUI] Failed to get session info: {e}")
-        return HTTPException(status_code=500, detail="Failed to get session info")
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get session info")
 
     try:
         main_data["scanner_status"] = {"state": scan.SCAN_PROCESS_STATE, "total_items": scan.SCAN_TOTAL_ITEMS, "done_items": scan.SCAN_DONE_ITEMS}
     except Exception as e:
         log.error(f"[GUI] Failed to get scanner info: {e}")
-        return HTTPException(status_code=500, detail="Failed to get scanner info")
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to get scanner info")
 
     return main_data
 
@@ -59,5 +63,5 @@ async def dashboard_user_stats():
     user_data = await utils.fetch_indexer_user_data()
     if not user_data:
         # we don't log the error to console here because fetch_indexer_user_data() does for us
-        return HTTPException(status_code=500, detail="Failed to fetch user stats")
+        raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to fetch user stats")
     return user_data
