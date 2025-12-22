0c1e436259d3fc15acbb7dce75c46ab27784bc6b
David Hume <xenophilicy@gmail.com>
2025-12-21 22:54:33 -0600

    feat(core): send local torrent name during server syncs

--- src/privateindexer_client/core/sync.py
+++ src/privateindexer_client/core/sync.py
@@ -19,7 +19,7 @@ async def periodic_sync_task():
             before = datetime.datetime.now()
 
             # gather minimal data from database for each torrent from database for sync
-            local_torrents = await database.fetch_all("SELECT id, infohash FROM torrents")
+            local_torrents = await database.fetch_all("SELECT id, name, infohash FROM torrents")
             total = len(local_torrents)
             log.info(f"[SYNC] Syncing {total} torrents with indexer")
 
