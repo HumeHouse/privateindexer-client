b22ef0abdc7f3f65ad960f619944dcdaf3742bd1
David Hume <xenophilicy@gmail.com>
2026-01-26 16:16:03 -0600

    feat(gui): move GUI specific code to separate helper

--- src/privateindexer_client/core/qbit_translator.py
+++ src/privateindexer_client/core/qbit_translator.py
@@ -3,7 +3,6 @@ import os
 import libtorrent as lt
 
 from privateindexer_client.core import database, config
-from privateindexer_client.core.logger import log
 
 
 def calc_eta(status: lt.torrent_status) -> int:
@@ -49,58 +48,6 @@ def map_state(status: lt.torrent_status) -> str:
     return torrent_state
 
 
-def format_peer_flags(peer: lt.peer_info) -> list[tuple[str, str]]:
-    """
-    Convert a libtorrent peer_info.flags into a qBittorrent-style string
-    https://web.archive.org/web/20141111072948/http://www.utorrent.com/help/faq/misc#faq13
-    """
-    flags = []
-
-    # downloading states
-    if peer.flags & peer.interesting:
-        if peer.flags & peer.remote_choked:
-            flags.append(("d", "Trying to download - Interested (local) & Choked (peer)"))
-        else:
-            flags.append(("D", "Downloading - Interested (local) & Unchoked (peer)"))
-
-    # uploading states
-    if peer.flags & peer.remote_interested:
-        if peer.flags & peer.choked:
-            flags.append(("u", "Not uploading - Interested (peer) & Choked (local)"))
-        else:
-            flags.append(("U", "Uploading - Interested (peer) & Unchoked (local)"))
-
-    if peer.flags & peer.optimistic_unchoke:
-        flags.append(("O", "Optimistic unchoke"))
-
-    if peer.flags & peer.snubbed:
-        flags.append(("S", "Peer is snubbed"))
-
-    if not (peer.flags & peer.outgoing_connection):
-        flags.append(("I", "Incoming connection"))
-
-    # unchoked by peer but we're not interested
-    if not (peer.flags & peer.interesting) and not (peer.flags & peer.remote_choked):
-        flags.append(("K", "Not downloading - Not interested (local) & Unchoked (peer)"))
-
-    # we unchoked them but they’re not interested
-    if not (peer.flags & peer.remote_interested) and not (peer.flags & peer.choked):
-        flags.append(("?", "Not uploading - Not interested (peer) & Unchoked (local)"))
-
-    # encrypted
-    if peer.flags & peer.rc4_encrypted:
-        flags.append(("E", "Encrypted traffic"))
-    elif peer.flags & peer.plaintext_encrypted:
-        flags.append(("e", "Encrypted handshake"))
-
-    # uTP
-    # the enum is missing for some reason in libtorrent python bindings
-    if peer.flags & (1 << 17):
-        flags.append(("P", "Peer using uTP"))
-
-    return flags
-
-
 async def map_torrents_to_qbit(client_torrents: list, category_filter: str = None) -> dict:
     """
     Convert a libtorrent.torrent_status object into a qBittorrent-compatible dict
@@ -132,23 +79,10 @@ async def map_torrents_to_qbit(client_torrents: list, category_filter: str = Non
         # we want to show the name in the database, not the internal torrent name - it's usually ugly (we use the internal one as a fallback)
         torrent_name = name_hash_map.get(torrent_hash, status.name)
 
-        mapped = {"added_on": int(status.added_time or 0), "content_path": os.path.join(status.save_path, status.name), "dlspeed": status.download_payload_rate,
-                  "download_path": status.save_path, "downloaded": status.all_time_download, "downloaded_session": status.total_payload_download, "eta": calc_eta(status),
-                  "hash": torrent_hash, "name": torrent_name, "num_complete": status.num_complete, "num_incomplete": status.num_incomplete,
-                  "num_leechs": max(0, status.num_peers - status.num_seeds), "num_seeds": status.num_seeds, "progress": round(status.progress, 3),
-                  "ratio": safe_ratio(status), "ratio_limit": -1, "save_path": status.save_path, "seeding_time": int(status.seeding_duration.total_seconds()),
-                  "seeding_time_limit": -1, "size": status.total_wanted, "state": map_state(status), "uploaded": status.all_time_upload,
-                  "uploaded_session": status.total_payload_upload, "upspeed": status.upload_payload_rate, "category": category, }
-
-        peers_list = []
-        for p in torrent.get_peer_info():
-            peers_list.append({"ip": p.ip[0], "port": p.ip[1], "client": p.client.decode("utf-8", errors="ignore") if isinstance(p.client, bytes) else str(p.client),
-                               "flags": format_peer_flags(p), "up_speed": p.payload_up_speed, "down_speed": p.payload_down_speed, "progress": p.progress, })
-        mapped["peers"] = peers_list
-
-        # we only ever have one tracker, ours, so process the first in the list
-        tracker_data = next(iter(torrent.trackers()))
-        mapped["tracker"] = {"verified": tracker_data["verified"], "next_announce": tracker_data.get("next_announce")}
+        mapped = {"content_path": os.path.join(status.save_path, status.name), "eta": calculate_eta(status), "hash": torrent_hash, "name": torrent_name,
+                  "progress": round(status.progress, 3), "ratio": safe_ratio(status), "ratio_limit": -1, "save_path": status.save_path,
+                  "seeding_time": int(status.seeding_duration.total_seconds()), "seeding_time_limit": -1, "size": status.total_wanted, "state": map_state(status),
+                  "category": category, }
 
         all_mapped_torrents.append(mapped)
 
@@ -160,17 +94,8 @@ def map_stats_to_qbit(stats_now: dict[str, int] | None, time_now: float | None, 
     """
     Converts the raw data from a current and previous update of libtorrent stats to match what qbit would normally return in an API request
     """
-    mapped = {}
-
-    # session totals
-    total_download = stats_now["net.recv_payload_bytes"] if stats_now else 0
-    total_upload = stats_now["net.sent_payload_bytes"] if stats_now else 0
-    mapped["dl_info_data"] = total_download
-    mapped["up_info_data"] = total_upload
-
     # all-time totals
-    mapped["alltime_dl"] = all_time_download
-    mapped["alltime_ul"] = all_time_upload
+    mapped = {"alltime_dl": all_time_download, "alltime_ul": all_time_upload}
 
     # global ratio is UL/DL if UL>0
     if all_time_upload > 0:
@@ -183,6 +108,9 @@ def map_stats_to_qbit(stats_now: dict[str, int] | None, time_now: float | None, 
         # offset the interval based on the previous timestamp
         interval = max(time_now - time_prev, 1e-6)
 
+        total_download = stats_now["net.recv_payload_bytes"] if stats_now else 0
+        total_upload = stats_now["net.sent_payload_bytes"] if stats_now else 0
+
         prev_download = stats_prev.get("net.recv_payload_bytes", 0)
         prev_upload = stats_prev.get("net.sent_payload_bytes", 0)
 
@@ -192,8 +120,8 @@ def map_stats_to_qbit(stats_now: dict[str, int] | None, time_now: float | None, 
         mapped["dl_info_speed"] = 0
         mapped["up_info_speed"] = 0
 
-    # base the connection status on the number of connections or if there is incoming traffic
-    if (stats_now and stats_now.get("net.has_incoming_connections", 0)) or mapped["up_info_speed"] > 0:
+    # base the connection status on the number of connections
+    if stats_now and stats_now.get("net.has_incoming_connections", 0):
         mapped["connection_status"] = "connected"
     else:
         mapped["connection_status"] = "disconnected"
